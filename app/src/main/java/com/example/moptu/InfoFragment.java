598a2856677dc1a16c819a24213503920faeaacd
koshimazaki <koshi@mazaki.com>
2024-06-13 22:57:22 +0200

    Created looks for title_frag.xml

---
+++ app/src/main/java/com/example/moptu/InfoFragment.java
@@ -0,0 +1,4 @@
+package com.example.moptu;
+
+public class InfoFragment {
+}
--- app/src/main/java/com/example/moptu/MainActivity.kt
+++ app/src/main/java/com/example/moptu/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.moptu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var frags: List<Fragment>
+    lateinit var toolbar: MaterialToolbar
+    lateinit var currFrag: Int
+    lateinit var selectedSeries: Int
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.navbar)
+        currFrag = 0
+
+        frags = ListOf(
+            TitleFragment(),
+            InfoFragment(),
+            ImageFragment()
+        )
     }
 }
